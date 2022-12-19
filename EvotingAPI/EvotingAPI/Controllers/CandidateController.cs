dd028f622917e1377b577a32764764eaaf9f0e21
Nitish <nitishrocker220@gmail.com>
2022-12-19 22:06:20 +0545

    Image Stored To server Path

--- EvotingAPI/EvotingAPI/Controllers/CandidateController.cs
+++ EvotingAPI/EvotingAPI/Controllers/CandidateController.cs
@@ -4,6 +4,13 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Reflection;
+using System.IO;
+using static System.Net.WebRequestMethods;
+using System.Text;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Reflection.Metadata;
 
 namespace EvotingAPI.Controllers
 {
@@ -14,7 +21,7 @@ namespace EvotingAPI.Controllers
         private readonly string _connstring;
         private readonly ILogger<CandidateController> _logger;
         private readonly IDapperService _dapperService;
-        public CandidateController(IConfiguration configuration, ILogger<CandidateController> logger,IDapperService dapperService)
+        public CandidateController(IConfiguration configuration, ILogger<CandidateController> logger, IDapperService dapperService)
         {
             _connstring = configuration.GetConnectionString("DbString");
             _logger = logger;
@@ -29,6 +36,13 @@ namespace EvotingAPI.Controllers
             string sql = @"Select * from Candidate";
             var list = _dapperService.Query<CandidateModel>(sql).ToList();
             _logger.LogInformation("Fetching complete");
+            foreach(var item in list)
+            {
+                var getCandidatePhoto = encodeToBase64(item.candidateFirstName, item.candidateLastName, "CandidatePhoto");
+                var getCandidatePartySymbol = encodeToBase64(item.candidateFirstName, item.candidateLastName, "CandidatePartysymbol");
+                item.candidatePhoto = getCandidatePhoto;
+                item.candidatePartySymbol = getCandidatePartySymbol;
+            }
             #region oldcode
             //SqlConnection con = new SqlConnection(_connstring);
             //SqlCommand cmd = new SqlCommand(sql, con);
@@ -50,12 +64,14 @@ namespace EvotingAPI.Controllers
         }
         [HttpPost]
         [Route("AddCandidate")]
-        public  bool CreateCandidate([FromBody] CandidateModel model)
+        public bool CreateCandidate([FromBody] CandidateModel model)
         {
-            string sql = @"Insert into Candidate values(@CandidateName,@CandidatePhoto,@CandidatePartyName,@CandidatePartySymbol)";
+            string sql = @"Insert into Candidate values(@CandidateFirstName,@CandidateLastName,@CandidatePartyName)";
             _logger.LogInformation("Adding candidate");
             var parameters = _dapperService.AddParam(model);
-            var affectedRows = _dapperService.Execute(sql,parameters);
+            var uploadCandidatePhoto = DecodeBase64String(model.candidatePhoto,"CandidatePhoto",model.candidateFirstName,model.candidateLastName);
+            var uploadCandidatePartySymbol = DecodeBase64String(model.candidatePartySymbol,"CandidatePartySymbol", model.candidateFirstName, model.candidateLastName); 
+            var affectedRows = _dapperService.Execute(sql, parameters);
             _logger.LogInformation("Candidate added");
             #region oldcode
             //SqlConnection con = new SqlConnection(_connstring);
@@ -68,10 +84,10 @@ namespace EvotingAPI.Controllers
             //int affectedRows = cmd.ExecuteNonQuery();
             //con.Close();
             #endregion
-            if (affectedRows > 0)
-                return true;
-            else
+            if (affectedRows <= 0 && !uploadCandidatePhoto && !uploadCandidatePartySymbol)
                 return false;
+            else
+                return true;
 
         }
         [HttpPost]
@@ -81,7 +97,7 @@ namespace EvotingAPI.Controllers
             string sql = @"Delete From Candidate where CandidateId =@id";
             _logger.LogInformation("Deleteing Candidate");
             var parameter = _dapperService.AddParam(id);
-            var affectedRows = _dapperService.Execute(sql,parameter);
+            var affectedRows = _dapperService.Execute(sql, parameter);
             _logger.LogInformation("Deleted");
             if (affectedRows > 0)
                 return true;
@@ -95,11 +111,34 @@ namespace EvotingAPI.Controllers
             _logger.LogInformation("Updating candidate");
             var parameters = _dapperService.AddParam(model);
             parameters.Add("@id", model.candidateId);
-            var affectedRows = _dapperService.Execute(sql,parameters);
+            var affectedRows = _dapperService.Execute(sql, parameters);
             _logger.LogInformation("Updated");
             if (affectedRows > 0)
                 return true;
             return false;
+        }
+        private bool DecodeBase64String(string encodedString, string FolderName,string FirstName , string LastName)
+        {
+            string FileName = FirstName + LastName + ".jpg";
+            byte[] decodedBytes = Convert.FromBase64String(encodedString);
+            
+            var path = Path.Combine(Directory.GetCurrentDirectory(), FolderName, FileName);
+            using (MemoryStream ms = new MemoryStream(decodedBytes))
+            {
+                Image image = Image.FromStream(ms);
+                image.Save(path, ImageFormat.Jpeg);
+            }
+            //System.IO.File.WriteAllBytes(path,decodedBytes);
+            return true;
+        }
+        private string encodeToBase64(string FirstName , string LastName , string FolderName)
+        {
+            var fileName = FirstName + LastName + ".jpg";
+            string encodedImage = "";
+            var path = Path.Combine(Directory.GetCurrentDirectory(), FolderName, fileName);
+            byte[] filebytes = System.IO.File.ReadAllBytes(path);
+            encodedImage = Convert.ToBase64String(filebytes);
+            return encodedImage;
         }
 
     }
--- EvotingAPI/EvotingAPI/Model/CandidateModel.cs
+++ EvotingAPI/EvotingAPI/Model/CandidateModel.cs
@@ -1,12 +1,16 @@
-﻿namespace EvotingAPI.Model
+﻿using System.Drawing;
+
+namespace EvotingAPI.Model
 {
     public class CandidateModel
     {
         public int candidateId { get; set; }
-        public string candidateName { get; set; }
+        public string candidateFirstName { get; set; }
+        public string candidateLastName { get; set; }
+
         public string candidatePhoto { get; set; }
         public string candidatePartyName { get; set; }
         public string candidatePartySymbol { get; set; }
-
+        
     }
 }
