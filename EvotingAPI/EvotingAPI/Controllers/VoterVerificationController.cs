5da3eca4f57fcc0bc7def8a827eb1f492d5a1099
Nitish <nitishrocker220@gmail.com>
2023-02-03 22:00:26 +0545

    Error handling implemented

--- EvotingAPI/EvotingAPI/Controllers/VoterVerificationController.cs
+++ EvotingAPI/EvotingAPI/Controllers/VoterVerificationController.cs
@@ -20,17 +20,29 @@ namespace EvotingAPI.Controllers
         [Route("VerifyVoterId")]
         public IActionResult VerifyId([FromBody] VoterModel model)
         {
-            string sql = @"Select VoterId,CitizenshipId from VoterList where VoterId=@VoterId And CitizenshipId=@CitizenshipId";
-            var param = _dapperService.AddParam(model);
-            _logger.LogInformation("Checking Voter exists or not");
-            var result = _dapperService.Query<VoterModel>(sql, param).ToList();
-            if(result.Count==1)
+            try
             {
-                _logger.LogInformation("voter exists");
-                return Ok("Verified");
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                string sql = @"Select VoterId,CitizenshipId from VoterList where VoterId=@VoterId And CitizenshipId=@CitizenshipId";
+                var param = _dapperService.AddParam(model);
+                _logger.LogInformation("Checking Voter exists or not");
+                var result = _dapperService.Query<VoterModel>(sql, param).ToList();
+                if (result.Count == 1)
+                {
+                    _logger.LogInformation("voter exists");
+                    return Ok("Verified");
+                }
+                _logger.LogInformation("voter does not exists");
+                return BadRequest("Not Verified");
             }
-            _logger.LogInformation("voter does not exists");
-            return BadRequest("Not Verified");
+            catch(Exception e)
+            {
+                _logger.LogInformation("Exception:{0}",e);
+                return BadRequest("Exception occured");
+            }
         }
 
     }
