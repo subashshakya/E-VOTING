814bc9ce4f1bb60c531b741e1fa14805d189680d
Nitish <nitishrocker220@gmail.com>
2023-02-13 21:59:54 +0545

    Romanized property added

--- EvotingAPI/EvotingAPI/Model/CandidateModel.cs
+++ EvotingAPI/EvotingAPI/Model/CandidateModel.cs
@@ -7,11 +7,17 @@ namespace EvotingAPI.Model
         public int candidateId { get; set; }
         public string candidateFirstName { get; set; }
         public string candidateLastName { get; set; }
-
         public string candidatePhoto { get; set; }
         public string candidatePartyName { get; set; }
         public string candidatePartySymbol { get; set; }
         public string NominatedYear { get; set; }
+        public string RomanizedCandidateFirstName { get; set; }
+        public string RomanizedCandidateLastName  { get; set; }
+        public string RomanizedCandidatePartyName { get; set; }
 
+    }
+    public class CandidateIdmodel
+    {
+        public int candidateId { get; set; }
     }
 }
